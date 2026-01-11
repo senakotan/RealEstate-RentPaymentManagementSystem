e33060f83a537da74992e5dbd042fc899d15f131
Sena Kotan <senakotan34@gmail.com>
2026-01-11 16:47:13 +0300

    Update KiraOdeme.cs

--- backend/backend/Models/KiraOdeme.cs
+++ backend/backend/Models/KiraOdeme.cs
@@ -5,15 +5,15 @@ namespace EmlakYonetimAPI.Models
         public int KiraOdemeID { get; set; }
         public int KiraSozlesmeID { get; set; }
 
-        public DateTime VadeTarihi { get; set; } // Ödenmesi gereken tarih
-        public DateTime? OdemeTarihi { get; set; } // Gerçekleþen tarih
+        public DateTime VadeTarihi { get; set; } 
+        public DateTime? OdemeTarihi { get; set; } 
 
         public decimal Tutar { get; set; }
         public int ParaBirimiID { get; set; }
 
-        public int OdemeDurumID { get; set; } // 1: Paid, 2: Late, 3: Pending
-        public int? OdemeYontemID { get; set; } // 1: Havale, 2: Nakit vs.
-
+        public int OdemeDurumID { get; set; } 
+        public int? OdemeYontemID { get; set; } 
+        
         public string? Aciklama { get; set; }
         public DateTime OlusturmaTarihi { get; set; }
     }
