e9ff3fb910e57e66ecdbb5630c90d163069df841
Sena Kotan <senakotan34@gmail.com>
2026-01-11 16:48:49 +0300

    Update ParaBirimi.cs

--- backend/backend/Models/ParaBirimi.cs
+++ backend/backend/Models/ParaBirimi.cs
@@ -3,7 +3,7 @@ namespace EmlakYonetimAPI.Models
     public class ParaBirimi
     {
         public int ParaBirimiID { get; set; }
-        public string Kod { get; set; } = string.Empty; // TRY, USD...
+        public string Kod { get; set; } = string.Empty; 
         public string Ad { get; set; } = string.Empty;
         public string Sembol { get; set; } = string.Empty;
     }
